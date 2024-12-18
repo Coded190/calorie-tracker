6d4c95d619c005c6a84530b7135e46c0e3bb8d72
Christian Moore <moorechristian15@yahoo.com>
2024-12-18 18:31:36 -0500

    changed database from MySQL to PostgreSQL

--- main.go
+++ main.go
@@ -2,167 +2,92 @@ package main
 
 import (
 	"database/sql"
+	//"context"
     "fmt"
     "log"
     "os"
 	"html/template"
 	"net/http"
 	"strconv"
-	"github.com/joho/godotenv"
+	//"github.com/joho/godotenv"
 
-	"github.com/go-sql-driver/mysql"
+	//"github.com/go-sql-driver/mysql"
+	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
 var db *sql.DB
 var curCalGoal int
 
 func main() {
+    connString := "postgres://chris13:@localhost:5432/food_data"
 
-	// Loading the .env file in my local development environment
-    if os.Getenv("PORT") == "" {
-        err := godotenv.Load(".env")
-        if err != nil {
-            panic(err) 
-        }
+    var err error
+    db, err = sql.Open("pgx", connString)
+    if err != nil {
+        log.Fatal("Error connecting to database: ", err)
     }
 
-	log.Println("Starting server...")
+    err = db.Ping()
+    if err != nil {
+        log.Fatal("Error pinging database: ", err)
+    }
+    fmt.Println("Connected to PostgreSQL")
 
-	fs := http.FileServer(http.Dir("static"))
+    // File server for static assets
+    fs := http.FileServer(http.Dir("static"))
     http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-	http.HandleFunc("/", login)
-	http.HandleFunc("/sign_up", sign_up)
-	http.HandleFunc("/login_user", login_user)
-	http.HandleFunc("/sign_up_user", sign_up_user)
-	http.HandleFunc("/results", results)
-	http.HandleFunc("/home", home)
-	http.HandleFunc("/update_max_cal_goal", update_max_cal_goal)
+    // Route handlers
+    http.HandleFunc("/", login)
+    http.HandleFunc("/sign_up", sign_up)
+    http.HandleFunc("/login_user", login_user)
+    http.HandleFunc("/sign_up_user", sign_up_user)
+    http.HandleFunc("/results", results)
+    http.HandleFunc("/home", home)
+    http.HandleFunc("/update_max_cal_goal", update_max_cal_goal)
 
+    // Starting the server
     port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
     log.Println("Starting server on port", port)
-    err := http.ListenAndServe(":"+port, nil)
+    err = http.ListenAndServe(":"+port, nil)
     if err != nil {
         log.Fatal("ListenAndServe failed:", err)
     }
-
-	/*
-	
-    // Start DB logic and Capture connection properties.
-    cfg := mysql.Config{
-        User:   os.Getenv("DBUSER"),
-        Passwd: os.Getenv("DBPASS"),
-        Net:    "tcp",
-        Addr:   "127.0.0.1:3306",
-        DBName: "food_data",
-		AllowNativePasswords: true,
-    }
-
-
-	
-    // Get a database handle.
-    var err error
-    db, err = sql.Open("mysql", cfg.FormatDSN())
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    pingErr := db.Ping()
-    if pingErr != nil {
-        log.Fatal(pingErr)
-    }
-    fmt.Println("Connected!")
-
-
-
-
-
-
-	firstName, lastName, maxCalorieGoal := greetUser()
-	curCalorieGoal := maxCalorieGoal
-	fmt.Printf("Hello, %s %s, let's get you to your calorie goal of %d calories!\n", firstName, lastName, curCalorieGoal)
-
-	for {
-		food, calories, day := getUserFood()
-		curCalorieGoal -= calories
-
-		totalCalories, err := calculateTotalCaloriesForDay(day)
-		if err != nil {
-			log.Fatal("Error calculating total calories: ", err)
-		}
-
-		err = updateTotalDayCalories(day, totalCalories)
-		if err != nil {
-			log.Fatal("Error updating total_day_calorie table: ", err)
-		}
-
-		fmt.Printf("Total calories for %s: %d\n", day, totalCalories)
-
-		if curCalorieGoal > 0 {
-			fmt.Printf("The %s you ate left you with %d calories left to reach your goal\n", food, curCalorieGoal)
-		} else {
-			fmt.Printf("Congrats you have reached your goal of %d calories\n", maxCalorieGoal)
-			fmt.Printf("The %s you ate put you over your goal by %d calories\n", food, curCalorieGoal * -1)
-			break
-		}
-	}
-
 	defer db.Close()
-
-	*/
 }
 
 
 func add_user(username string, password string, maxCalorieGoal int) bool {
-	cfg := mysql.Config{
-        User:   os.Getenv("DBUSER"),
-        Passwd: os.Getenv("DBPASS"),
-        Net:    "tcp",
-        Addr:   "127.0.0.1:3306",
-        DBName: "food_data",
-		AllowNativePasswords: true,
-    }
-
-	db,err := sql.Open("mysql", cfg.FormatDSN())
-	if err != nil {
-		panic(err)
-	}
-
-	add, err := db.Query("INSERT INTO Users(Name, Password, MaxCalorieGoal) VALUES (?, ?, ?)", (username), (password), (maxCalorieGoal))
+	add, err := db.Query("INSERT INTO Users(Name, Password, MaxCalorieGoal) VALUES ($1, $2, $3)", (username), (password), (maxCalorieGoal))
 	if err != nil {
 		panic(err)
 	}
 
 	fmt.Println(add)
-	defer db.Close()
 	return true
 }
 
 func check_user(username string, password string) bool {
-	cfg := mysql.Config{
-        User:   os.Getenv("DBUSER"),
-        Passwd: os.Getenv("DBPASS"),
-        Net:    "tcp",
-        Addr:   "127.0.0.1:3306",
-        DBName: "food_data",
-		AllowNativePasswords: true,
-    }
-
-	db,err := sql.Open("mysql", cfg.FormatDSN())
+	rows, err := db.Query("SELECT username, password FROM users WHERE username = $1 AND password = $2", username, password)
 	if err != nil {
-		panic(err)
+		log.Fatal("Error querying users table: ", err)
 	}
 
-	var exists bool
-	var query string
-	query = fmt.Sprintf("SELECT EXISTS(SELECT Name FROM Users WHERE Name='%s' AND Password='%s')", (username),(password))
-	row := db.QueryRow(query).Scan(&exists)
-	if err != nil && err != sql.ErrNoRows {
-		panic(err)
+	for rows.Next() {
+		var dbUsername string
+		var dbPassword string
+
+		err := rows.Scan(&dbUsername, &dbPassword)
+		if err != nil {
+			fmt.Println("User not found: ", err)
+			return false
+		}
 	}
-	fmt.Println(row)
-	defer db.Close()
-	return exists
+	defer rows.Close()
+	return true
 }
 
 
@@ -229,9 +154,9 @@ func sign_up_user(w http.ResponseWriter, r *http.Request){
 
 func login_user(w http.ResponseWriter, r *http.Request){
 	r.ParseForm()
-	var username = r.Form["fname"]
+	var username = r.Form["username"]
 	var password = r.Form["Password"]
-	fmt.Println(username, " ", password)
+	fmt.Println(username[0], " ", password[0])
 
 	if (check_user(username[0], password[0])) {
 		var tmplate = template.Must(template.ParseFiles("templates/index.html"))
@@ -244,27 +169,20 @@ func login_user(w http.ResponseWriter, r *http.Request){
 }
 
 func get_curCalGoal(name string) {
-	cfg := mysql.Config{
-        User:   os.Getenv("DBUSER"),
-        Passwd: os.Getenv("DBPASS"),
-        Net:    "tcp",
-        Addr:   "127.0.0.1:3306",
-        DBName: "food_data",
-		AllowNativePasswords: true,
-    }
-
-	db,err := sql.Open("mysql", cfg.FormatDSN())
+	rows, err := db.Query("SELECT max_calorie_goal FROM users WHERE username = $1", name)
 	if err != nil {
-		panic(err)
+		log.Fatal("Error querying users table: ", err)
 	}
 
-	// Updates curCalGoal with max calorie for user
-	err = db.QueryRow("SELECT MaxCalorieGoal FROM Users WHERE Name = ?", name).Scan(&curCalGoal)
-	if err != nil {
-		panic(err)
+	for rows.Next() {
+		var dbUsername string
+
+		err := rows.Scan(&dbUsername)
+		if err != nil {
+			log.Fatal("Error scanning rows: ", err)
+		}
 	}
-
-
+	defer rows.Close()
 }
 
 func greetUser() (string, string, int) {
@@ -295,7 +213,7 @@ func getUserFood() (string, int, string) {
 	fmt.Println("Enter the number of calories in the food: ")
 	fmt.Scan(&calories)
 
-	_, err := db.Exec("INSERT INTO calorie_stats (day, food_item, calorie_amount) VALUES (?, ?, ?)", day, food, calories)
+	_, err := db.Exec("INSERT INTO calorie_stats (day, food_item, calorie_amount) VALUES ($1, $2, $3)", day, food, calories)
 	if err != nil {
 		log.Fatal("Error inserting into calorie_stats table: ", err)
 	}
@@ -305,7 +223,7 @@ func getUserFood() (string, int, string) {
 // Sums the total calories for a given day
 func calculateTotalCaloriesForDay(day string) (int, error) {
 	var totalCalories int
-	err := db.QueryRow("SELECT SUM(calorie_amount) FROM calorie_stats WHERE day = ?", day).Scan(&totalCalories)
+	err := db.QueryRow("SELECT SUM(calorie_amount) FROM calorie_stats WHERE day = $1", day).Scan(&totalCalories)
 	if err != nil {
 		return 0, err
 	}
@@ -316,8 +234,8 @@ func calculateTotalCaloriesForDay(day string) (int, error) {
 func updateTotalDayCalories(day string, totalCalories int) error {
 	_, err := db.Exec(`
 		INSERT INTO total_day_calorie (day, total_calorie_amount) 
-		VALUES (?, ?) 
-		ON DUPLICATE KEY UPDATE total_calorie_amount = ?`, day, totalCalories, totalCalories)
+		VALUES ($1, $2) 
+		ON DUPLICATE KEY UPDATE total_calorie_amount = $3`, day, totalCalories, totalCalories)
 	return err
 }
 
@@ -341,65 +259,34 @@ func update_max_cal_goal(w http.ResponseWriter, r *http.Request) {
 }
 
 func update_max_calorie_goal(fname string, maxCalGoal int) bool {
-	cfg := mysql.Config{
-        User:   os.Getenv("DBUSER"),
-        Passwd: os.Getenv("DBPASS"),
-        Net:    "tcp",
-        Addr:   "127.0.0.1:3306",
-        DBName: "food_data",
-		AllowNativePasswords: true,
-    }
-
-	db,err := sql.Open("mysql", cfg.FormatDSN())
-	if err != nil {
-		panic(err)
-	}
-
-	_, err = db.Exec(`UPDATE Users SET MaxCalorieGoal = ? WHERE Name = ?`, maxCalGoal, fname)
+	_, err := db.Exec(`UPDATE Users SET MaxCalorieGoal = $1 WHERE Name = $2`, maxCalGoal, fname)
 	if err != nil {
 		return false
 	}
 
-	defer db.Close()
 	return true
 
 }
 
 func calculate_total_calories(date string, food_item string, calories int) bool {
-	cfg := mysql.Config{
-        User:   os.Getenv("DBUSER"),
-        Passwd: os.Getenv("DBPASS"),
-        Net:    "tcp",
-        Addr:   "127.0.0.1:3306",
-        DBName: "food_data",
-		AllowNativePasswords: true,
-    }
-
-	db,err := sql.Open("mysql", cfg.FormatDSN())
-	if err != nil {
-		panic(err)
-	}
-
 	// Update calorie_stats table
-	_, err = db.Exec("INSERT INTO calorie_stats (day, food_item, calorie_amount) VALUES (?, ?, ?)", date, food_item, calories)
+	_, err := db.Exec("INSERT INTO calorie_stats (day, food_item, calorie_amount) VALUES ($1, $2, $3)", date, food_item, calories)
 	if err != nil {
 		return false
 	}
 
 	// Grabs the total calories from a specific day
 	var total_calories int
-	err = db.QueryRow("SELECT SUM(calorie_amount) FROM calorie_stats WHERE day = ?", date).Scan(&total_calories)
+	err = db.QueryRow("SELECT SUM(calorie_amount) FROM calorie_stats WHERE day = $1", date).Scan(&total_calories)
 	if err != nil {
 		return false
 	}
 	
 	// Updates total_day_calorie for specific day
-	_, err = db.Exec("INSERT INTO total_day_calorie (day, total_calorie_amount) VALUES (?, ?) ON DUPLICATE KEY UPDATE total_calorie_amount = ?", date, total_calories, total_calories)
+	_, err = db.Exec("INSERT INTO total_day_calorie (day, total_calorie_amount) VALUES ($1, $2) ON DUPLICATE KEY UPDATE total_calorie_amount = $3", date, total_calories, total_calories)
 	if err != nil {
 		return false
 	}
-
-	defer db.Close()
 
 	return true
 
